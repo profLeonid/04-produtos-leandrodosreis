d4c3a9e9074da5108116cf5059f84a6593f46172
LEANDRO DOS REIS FILHO <25203700@dsn127m1246734.cfp127aluno.local>
2026-02-24 15:54:54 -0300

    funcao de limpar input e validar vazio completas

--- main.js
+++ main.js
@@ -8,7 +8,34 @@ function adicionarProduto(){
 
     const span = document.createElement('span')
     span.textContent = produto.value
-    span.className = 'bg-yellow-500 px-8 py-2'
+    
+    
 
-    lista.appendChild(span)
+    let nomeProduto = validarProduto(produto.value)
+
+    if(nomeProduto){
+        lista.appendChild(span)
+        span.className = 'bg-yellow-500 px-8 py-2'
+
+    }else{
+        return false
+    }
+
+    limparCaixaInpt(produto.value)
+
+}
+
+function validarProduto(produto){
+
+    let nomeProduto = produto
+
+    if(nomeProduto == ''){
+        return false
+    }else{
+        return nomeProduto
+    }
+}
+
+function limparCaixaInpt(){
+    document.getElementById('produto').value = ''
 }
