5ef694a389a0abe63254a1cbd8a6727938e4cff6
LEANDRO DOS REIS FILHO <25203700@dsn127m1246734.cfp127aluno.local>
2026-02-24 17:00:48 -0300

    fazendo as funcoes do codigo e da quantidade

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
 'use strict'
 
-function adicionarProduto(){
-    const produto = document.getElementById('produto')
-    console.log(produto.value)
-    const lista = document.getElementById('lista')
-    //lista.textContent = produto.value
+// function adicionarProduto(){
+//     const produto = document.getElementById('produto')
+//     console.log(produto.value)
+//     const lista = document.getElementById('lista')
+//     //lista.textContent = produto.value
 
-    const span = document.createElement('span')
-    span.textContent = produto.value
+//     const span = document.createElement('span')
+//     span.textContent = produto.value
     
     
 
-    let nomeProduto = validarProduto(produto.value)
+//     let nomeProduto = validarProduto(produto.value)
 
-    if(nomeProduto){
-        lista.appendChild(span)
-        span.className = 'bg-yellow-500 px-8 py-2'
+//     if(nomeProduto){
+//         lista.appendChild(span)
+//         span.className = 'bg-yellow-500 px-8 py-2'
 
-    }else{
-        return false
-    }
+//     }else{
+//         return false
+//     }
 
-    limparCaixaInpt(produto.value)
+//     limparCaixaInpt(produto.value)
 
-}
+// }
 
 function validarProduto(produto){
 
@@ -36,6 +36,58 @@ function validarProduto(produto){
     }
 }
 
+
+function adicionarCodigo(){
+    const codigo = document.getElementById('codigo')
+    console.log(codigo.value)
+    const lista = document.getElementById('lista')
+    //lista.textContent = produto.value
+
+    const span2 = document.createElement('span')
+    span2.textContent = codigo.value
+    
+    
+
+    let sistCodigo = validarProduto(codigo.value)
+
+    if(sistCodigo){
+        lista.appendChild(span2)
+        span.className = 'bg-yellow-500 px-8 py-2'
+
+    }else{
+        return false
+    }
+
+    limparCaixaInpt(codigo.value)
+
+}
+
 function limparCaixaInpt(){
     document.getElementById('produto').value = ''
+    document.getElementById('codigo').value = ''
+    document.getElementById('quantidade').value = ''
 }
+// function adicionarProduto(){
+//     const produto = document.getElementById('produto')
+//     console.log(produto.value)
+//     const lista = document.getElementById('lista')
+//     //lista.textContent = produto.value
+
+//     const span = document.createElement('span')
+//     span.textContent = produto.value
+    
+    
+
+//     let nomeProduto = validarProduto(produto.value)
+
+//     if(nomeProduto){
+//         lista.appendChild(span)
+//         span.className = 'bg-yellow-500 px-8 py-2'
+
+//     }else{
+//         return false
+//     }
+
+//     limparCaixaInpt(produto.value)
+
+// }
